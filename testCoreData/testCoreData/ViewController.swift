82867ff9dbdd8a91406bb1daaf4da282aa8adb8f
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2019-11-21 22:43:33 +0700

    Add CoreData

---
+++ testCoreData/testCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testCoreData
+//
+//  Created by Trinh Thai on 11/13/19.
+//  Copyright © 2019 Trinh Thai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
